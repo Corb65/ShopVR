9195e072bd0b369d7810d2aa1d895a31a80a12d4
Corbin Dickens <corbin-dickens@live.nmit.ac.nz>
2016-10-05 19:18:47 +1300

    added external JS click functions

---
+++ click.js
@@ -0,0 +1,41 @@
+<script type="text/javascript">
+function onclicks(){
+
+	// Blue Methods
+	document.querySelector('#blueshirt').addEventListener('click', function() {
+		activeshirt.setAttribute('src', '#blue');
+		title.setAttribute('text', 'text: Product : Blue Shirt');
+	});
+
+	// Green Methods
+	document.querySelector('#greenshirt').addEventListener('click', function() {
+		activeshirt.setAttribute('src', '#green');
+		title.setAttribute('text', 'text: Product : Green Shirt');
+	});
+
+	// Orange Methods
+	document.querySelector('#orangeshirt').addEventListener('click', function() {
+		activeshirt.setAttribute('src', '#orange');
+		title.setAttribute('text', 'text: Product : Orange Shirt');
+	});
+
+	// Purple Methods
+	document.querySelector('#purpleshirt').addEventListener('click', function() {
+		activeshirt.setAttribute('src', '#purple');
+		title.setAttribute('text', 'text: Product : Purple Shirt');
+	});
+
+	// Red Methods
+	document.querySelector('#redshirt').addEventListener('click', function() {
+		activeshirt.setAttribute('src', '#red');
+		title.setAttribute('text', 'text: Product : Red Shirt');
+	});
+
+	// Yellow Methods
+	document.querySelector('#yellowshirt').addEventListener('click', function() {
+		activeshirt.setAttribute('src', '#yellow');
+		title.setAttribute('text', 'text: Product : Yellow Shirt');
+	});
+
+}
+</script>
